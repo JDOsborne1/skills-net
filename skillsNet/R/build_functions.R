e582473dcf049494cee02449840a09ac4183c398
JDO <atharos@vivaldi.net>
2019-08-25 19:44:36 +0100

    extended the root skill cluster  generation to include a date of addition

--- skillsNet/R/build_functions.R
+++ skillsNet/R/build_functions.R
@@ -3,17 +3,27 @@
 #' Make RS Cluster
 #'
 #' @param skill_base_name The base name of the skill, will be appended with the rs- prefix
+#' @param added_date The date that that base skill was added
 #' @param base_difficulty The base difficulty of that type of skill
 #'
 #' @return
 #' @export
 #'
 #' @examples
-makeRootSkillCluster <- function(skill_base_name, base_difficulty) {
+makeRootSkillCluster <- function(skill_base_name, base_difficulty, added_date = NULL) {
   skill_root_name <- paste0("rs-", skill_base_name)
   base_difficulty_typed <- paste0("nu-", base_difficulty)
+
+  added_date_typed <-paste0("da-", added_date)
+
   cluster_tibble <- tibble::tribble(~Origin, ~Relationship, ~Value,
                                     skill_root_name, "has.base", base_difficulty_typed)
+
+  if(!is.null(added_date)){
+    cluster_tibble <- rbind(cluster_tibble, tibble::tribble(~Origin, ~Relationship, ~Value,
+                                                            skill_root_name, "added.date", added_date_typed))
+  }
+
   return(cluster_tibble)
 }
 
