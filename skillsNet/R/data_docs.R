0df867c31f860c5d0fc76ca26ff8c6bdd39793b8
JDO <atharos@vivaldi.net>
2019-08-25 17:14:05 +0100

    adding in a testing dataset

---
+++ skillsNet/R/data_docs.R
@@ -0,0 +1,12 @@
+#' DS Skill stream test dataset
+#'
+#' A testing dataset which gives a triplet database for examples use
+#'
+#' @format A tibble with 15 obs. of 3 variables:
+#' \describe{
+#'   \item{Origin}{The root object, to which a value can be attached}
+#'   \item{Relationship}{The relationship tying the root to the value}
+#'   \item{Value}{The values tied into the object}
+#'   ...
+#' }
+"test_data"
